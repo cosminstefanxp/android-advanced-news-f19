753475f92b1fc7177b4ae22b1556cf0dde1568a6
Avramescu Mihai-Catalin <avramescumihaicatalin@gmail.com>
2019-12-17 23:02:24 +0200

    first attempt Retrofit implementation

--- app/src/main/java/ro/atelieruldigital/news/home/HomeActivity.java
+++ app/src/main/java/ro/atelieruldigital/news/home/HomeActivity.java
@@ -2,8 +2,15 @@ package ro.atelieruldigital.news.home;
 
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import ro.atelieruldigital.news.R;
 import ro.atelieruldigital.news.core.BaseActivity;
+import ro.atelieruldigital.news.model.NewsListResponse;
+import ro.atelieruldigital.news.model.ws.NewsWebService;
 
 public class HomeActivity extends BaseActivity {
 
@@ -11,5 +18,35 @@ public class HomeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        getDataFromServer();
+    }
+
+    private void getDataFromServer() {
+        NewsWebService newsWebService = new NewsWebService();
+        Call<NewsListResponse> newsListResponseCall = newsWebService.queryArticles("bitcoin");
+//        newsListResponseCall.enqueue(new Callback<NewsListResponse>() {
+//            @Override
+//            public void onResponse(Call<NewsListResponse> call, Response<NewsListResponse> response) {
+//                System.out.println("print: " + response.body().getStatus());
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<NewsListResponse> call, Throwable t) {
+//                System.out.println("failure: " );
+//
+//            }
+//        });
+
+        try {
+            Response<NewsListResponse> response = newsListResponseCall.execute();
+            NewsListResponse newsListResponse = response.body();
+            System.out.println("response: " + response.body().getStatus());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/ro/atelieruldigital/news/model/ws/NewsWebService.java
+++ app/src/main/java/ro/atelieruldigital/news/model/ws/NewsWebService.java
@@ -1,19 +1,37 @@
 package ro.atelieruldigital.news.model.ws;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import ro.atelieruldigital.news.App;
+import ro.atelieruldigital.news.R;
 import ro.atelieruldigital.news.model.NewsListResponse;
 
 public class NewsWebService {
     // TODO: Add functionality according to API
 
     // TODO: Load Token
-    private static final String API_KEY = "";
+    private static final String API_KEY = "534a091354c14911aa44a800e5270924";
     private NewsApi newsApi;
 
     public NewsWebService() {
         // TODO: Initialize newsApi with Retrofit
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://newsapi.org")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        NewsApi newsApi = retrofit.create(NewsApi.class);
+        this.newsApi = newsApi;
     }
 
     public Call<NewsListResponse> queryArticles(String searchString) {
